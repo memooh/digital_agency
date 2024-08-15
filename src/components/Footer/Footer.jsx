841f17c868f56b055d833e05d7bc4e66200b1ce8
memooh <m.ovdb@outlook.com>
2024-08-15 12:53:42 +0200

    Footer tablet responsive fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer className="bg-dark footerE text-light pt-5 pb-4">
           <div className="container">
-            <div className="row">
-              <div className="col-md-2 col-6 mb-4">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+              <div className="col mb-4">
                 <h5 className="text-uppercase">Company Info</h5>
                 <ul className="list-unstyled">
                   <li><a href="#" className="text-light">About Us</a></li>
@@ -15,7 +15,7 @@ const Footer = () => {
                 </ul>
               </div>
               
-              <div className="col-md-2 col-6 mb-4">
+              <div className="col mb-4">
                 <h5 className="text-uppercase">Legal</h5>
                 <ul className="list-unstyled">
                   <li><a href="#" className="text-light">About Us</a></li>
@@ -25,7 +25,7 @@ const Footer = () => {
                 </ul>
               </div>
               
-              <div className="col-md-2 col-6 mb-4">
+              <div className="col mb-4">
                 <h5 className="text-uppercase">Features</h5>
                 <ul className="list-unstyled">
                   <li><a href="#" className="text-light">Business Marketing</a></li>
@@ -35,7 +35,7 @@ const Footer = () => {
                 </ul>
               </div>
     
-              <div className="col-md-2 col-6 mb-4">
+              <div className="col mb-4">
                 <h5 className="text-uppercase">Resources</h5>
                 <ul className="list-unstyled">
                   <li><a href="#" className="text-light">iOS & Android</a></li>
@@ -45,7 +45,7 @@ const Footer = () => {
                 </ul>
               </div>
     
-              <div className="col-md-4 mb-4">
+              <div className="col mb-4">
                 <h5 className="text-uppercase">Get In Touch</h5>
                 <form>
                   <div className="mb-3">
@@ -58,14 +58,14 @@ const Footer = () => {
             </div>
             
             <div className="row">
-              <div className="col footerNotes  mt-4">
+              <div className="col footerNotes mt-4">
                 <hr className='mb-5'/>
                 <p>Made by Mehmet Ozturk | Design from Figma</p>
               </div>
             </div>
           </div>
         </footer>
-      );
+    );
 }
 
 export default Footer;
